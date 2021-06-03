31ad2b1178067b7e0a0766c2c66a15f240bc39cf
JamalRahman <jamalrahman95@gmail.com>
2021-06-03 11:06:32 +0100

    Added flake8

--- hybridtfidf/utils.py
+++ hybridtfidf/utils.py
@@ -1,52 +1,52 @@
 from numpy.linalg import norm
 from numpy import dot
 
+
 def cosine_sim(vec1, vec2):
-    '''Calculates the cosine similarity between two vectors
-    
+    """Calculates the cosine similarity between two vectors
+
     Args:
         vec1 (list of float): A vector
         vec2 (list of float): A vector
 
     Returns:
-        The cosine similarity between the two input vectors 
-    '''
-    return dot(vec1,vec2) / (norm(vec1)*norm(vec2))
+        The cosine similarity between the two input vectors
+    """
+    return dot(vec1, vec2) / (norm(vec1) * norm(vec2))
 
 
-def select_salient_posts(post_vectors,post_weights,k = 10,similarity_threshold = 0.4):
-    '''
+def select_salient_posts(post_vectors, post_weights, k=10, similarity_threshold=0.4):
+    """
         Selects the top k most salient posts in a collection of posts.
-        To avoid redundancy, any post too similar to other-posts are disregarded. Each selected post will therefore be both highly salient and representative of unique semantics.
+        To avoid redundancy, any post too similar to other-posts are disregarded. Each selected post will
+        therefore be both highly salient and representative of unique semantics.
 
         Note:
-            post_vectors and post_weights must be in the same order. The ith element of post_weights must reflect the ith element of post_vectors
+            post_vectors and post_weights must be in the same order. The ith element of post_weights must reflect
+             the ith element of post_vectors
 
         Args:
-            post_vectors (list of (list of float)): Hybrid tfidf representation of the documents as a document-term matrix
+            post_vectors (list of (list of float)): Hybrid tfidf representation of the documents
+             as a document-term matrix
 
             post_weights (list of float): Hybrid Tfidf weight for each document
 
             k (int): The number of posts to select as output
 
-            sim_threshold (float): The maximum cosine similiarity for a post to be selected
+            similarity_threshold (float): The maximum cosine similiarity for a post to be selected
 
-        Returns:
+    """
 
-    '''
-
-
-    sorted_keyed_vectors = [z for _, z in sorted(zip(post_weights,enumerate(post_vectors)), key=lambda i: i[0],reverse=True)] # z is (i,vi) sorted by weight
+    sorted_keyed_vectors = [z for _, z in sorted(zip(post_weights, enumerate(post_vectors)), key=lambda i: i[0],
+                                                 reverse=True)]  # z is (i,vi) sorted by weight
 
     i = 1
 
     veclength = len(post_vectors)
     loop_condition = True
 
-    significant_indices = []
-    significant_indices.append(0)
-    unsorted_indices = []
-    unsorted_indices.append(sorted_keyed_vectors[0][0])
+    significant_indices = [0]
+    unsorted_indices = [sorted_keyed_vectors[0][0]]
 
     while loop_condition:
         is_similar = False
@@ -60,8 +60,8 @@ def select_salient_posts(post_vectors,post_weights,k = 10,similarity_threshold =
             significant_indices.append(i)
             unsorted_indices.append(sorted_keyed_vectors[i][0])
 
-        if (len(significant_indices) >= k) or (i >= veclength-1) :
+        if (len(significant_indices) >= k) or (i >= veclength - 1):
             loop_condition = False
-        i+=1
-    
+        i += 1
+
     return unsorted_indices
--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup, find_packages
 
-
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-  name = 'hybridtfidf',
-  packages = find_packages(),
-  version = '1.0.6',
-  license='MIT',
-  description = 'An implementation of the Hybrid TF-IDF microblog summarisation algorithm as proposed by David Ionuye and Jugal K. Kalita.',
-  long_description = long_description,
-  long_description_content_type = 'text/markdown',
-  author = 'Jamal Rahman',
-  author_email = 'jamalrahman95@gmail.com', 
-  url = 'https://github.com/jamalrahman/hybridtfidf',
-  keywords = ['TFIDF', 'Text Summarization','NLP'],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',    
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',  
-    'Programming Language :: Python :: 3',
-  ],
+    name='hybridtfidf',
+    packages=find_packages(),
+    version='1.0.7',
+    license='MIT',
+    description='An implementation of the Hybrid TF-IDF microblog summarisation algorithm as proposed by David Ionuye '
+                'and Jugal K. Kalitaß.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jamal Rahman',
+    author_email='jamalrahman95@gmail.com',
+    url='https://github.com/jamalrahman/hybridtfidf',
+    keywords=['TFIDF', 'Text Summarization', 'NLP'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
 )
